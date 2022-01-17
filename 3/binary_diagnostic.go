5ad2537205a3d1f20d56190ed594fdb2dfa63d0e
Ignas <ign.budreika@gmail.com>
2022-01-17 02:17:53 +0200

    Finished day 3 part 1

---
+++ 3/binary_diagnostic.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type message struct {
+	Index           int
+	MostCommonByte  byte
+	LeastCommonByte byte
+}
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	numbers, err := parse("input.txt")
+	if err != nil {
+		log.Fatal(":(")
+	}
+
+	if len(numbers) == 0 {
+		fmt.Println("0")
+		os.Exit(1)
+	}
+
+	answers := make(chan message, len(numbers[0]))
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < len(numbers[0]); i++ {
+		wg.Add(1)
+		go countOnes(wg, numbers, i, answers)
+	}
+
+	wg.Wait()
+
+	gammaBytes := make([]string, len(numbers[0]))
+	epsilonBytes := make([]string, len(numbers[0]))
+
+	for i := 0; i < len(numbers[0]); i++ {
+		msg := <-answers
+		gammaBytes[msg.Index] = string(msg.MostCommonByte)
+		epsilonBytes[msg.Index] = string(msg.LeastCommonByte)
+	}
+
+	gammaRate, err := strconv.ParseInt(strings.Join(gammaBytes, ""), 2, 64)
+	if err != nil {
+		log.Fatalf("something wrong with gamma rate: %s", strings.Join(gammaBytes, ""))
+	}
+
+	epsilonRate, err := strconv.ParseInt(strings.Join(epsilonBytes, ""), 2, 64)
+	if err != nil {
+		log.Fatalf("something wrong with epsilon rate: %s", strings.Join(epsilonBytes, ""))
+	}
+
+	fmt.Printf("gamma rate: %d\n", gammaRate)
+	fmt.Printf("epsilon rate: %d\n", epsilonRate)
+	fmt.Printf("power consumption: %d\n", gammaRate*epsilonRate)
+}
+
+func part2() {
+	numbers, err := parse("input.txt")
+	if err != nil {
+		log.Fatal(":(")
+	}
+
+	if len(numbers) == 0 {
+		fmt.Println("0")
+		os.Exit(1)
+	}
+
+	answers := make(chan message, len(numbers[0]))
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < len(numbers[0]); i++ {
+		wg.Add(1)
+		go countOnes(wg, numbers, i, answers)
+	}
+
+	wg.Wait()
+
+	mostCommonBytes := make([]string, len(numbers[0]))
+	leastCommonBytes := make([]string, len(numbers[0]))
+
+	for i := 0; i < len(numbers[0]); i++ {
+		msg := <-answers
+		mostCommonBytes[msg.Index] = string(msg.MostCommonByte)
+		leastCommonBytes[msg.Index] = string(msg.LeastCommonByte)
+	}
+}
+
+func countOnes(wg *sync.WaitGroup, numbers []string, i int, answer chan<- message) {
+	defer wg.Done()
+	count := 0
+
+	for _, number := range numbers {
+		n, _ := strconv.Atoi(string(number[i]))
+
+		count += n
+	}
+
+	if count > len(numbers)/2 {
+		answer <- message{Index: i, MostCommonByte: '1', LeastCommonByte: '0'}
+	} else {
+		answer <- message{Index: i, MostCommonByte: '0', LeastCommonByte: '1'}
+	}
+}
+
+func parse(filename string) ([]string, error) {
+	numbers := make([]string, 0)
+
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return numbers, fmt.Errorf("uh oh reading diagnose went wrong: %v", err)
+	}
+
+	for _, line := range strings.Split(string(input), "\n") {
+		numbers = append(numbers, line)
+	}
+
+	return numbers, nil
+}
